6068ec78ef5d97c937d0dd51723e584143f584ac
Shota Iuchi <shtaiuchi.develop@gmail.com>
2020-07-10 00:51:37 +0900

    mxp

--- app/src/main/java/com/example/aac/ui/main/BackgroundFragment.kt
+++ app/src/main/java/com/example/aac/ui/main/BackgroundFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.aac.R
 import com.example.aac.databinding.BackgroundFragmentBinding
@@ -22,7 +24,7 @@ class BackgroundFragment : Fragment() {
     private val fragmentViewModel = activityViewModels<FragmentViewModel>()
     private val toastModel = activityViewModels<ToastViewModel>()
 
-    private val aacMxpViewModel : AacMxpViewModel by viewModels()
+    private val aacMxpViewModel : AacMxpViewModel by activityViewModels()
 
 
 
@@ -38,6 +40,14 @@ class BackgroundFragment : Fragment() {
         binding.aacMxpViewModel = aacMxpViewModel
         //binding.toast.setOnClickListener{fragmentViewModel.value.toast.postValue(true)}
         binding.toast.setOnClickListener{toastModel.value.doShow(ToastViewModel.Data("MMM", 10000))}
+
+        aacMxpViewModel.mxpHandle.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                // わんたいむ通知に帰る必要あり？
+                Toast.makeText(context, it.xxx.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+
         return binding.root
     }
 
